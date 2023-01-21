253d0aec4b4d96f72462632b0c2cb11179c3f62d
ElcaroNossam <agent_shops@i.ua>
2023-01-22 00:52:04 +0200

    registration user

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   get 'signup'  =>  'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
 end
