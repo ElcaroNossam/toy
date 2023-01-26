25ed560a6cc9aafffcbfd4672adb0454c64522e9
ElcaroNossam <agent_shops@i.ua>
2023-01-27 00:01:44 +0200

    login and loguot with coocies

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-    test "invalid signup information" do
+    
+  def setup
+    @user = users(:michael)
+  end
+
+  test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
@@ -24,6 +29,37 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    get logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    get logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
 end
