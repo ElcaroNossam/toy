3122327a2d8662863c99c3fe4bd0b9bfbdd0beaf
ElcaroNossam <agent_shops@i.ua>
2023-02-13 21:23:24 +0200

    Upluad picture with cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = "elcaronossam"
+    config.api_key = "212732786479357"
+    config.api_secret = "2N7O4mLYfPxojE1wJeERJZeLb1Q"
+    config.secure = true
+    config.cdn_subdomain = true
+   end
